2e7a71511855a79e4a6183b584580b880a85d0ab
ak4422920 <ak4422920@gmail.com>
2026-03-10 08:17:52 +0530

    Update router.py

--- bot/router.py
+++ bot/router.py
@@ -1,5 +1,5 @@
 from telegram.ext import CommandHandler, CallbackQueryHandler, MessageHandler, filters
-from bot.commands import start, quiz, challenge, createtournament, leaderboard, elite, profile, uploadpdf, receive_pdf
+from bot.commands import start, quiz, leaderboard, elite, generate
 from quiz_engines.inline_quiz import handle_answer
 from challenges.challenge_handlers import accept_challenge
 from pdf.pdf_handlers import handle_pdf_approval
@@ -10,12 +10,9 @@ def register_handlers(app):
 
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler("quiz", quiz))
-    app.add_handler(CommandHandler("challenge", challenge))
-    app.add_handler(CommandHandler("createtournament", createtournament))
     app.add_handler(CommandHandler("leaderboard", leaderboard))
     app.add_handler(CommandHandler("elite", elite))
-    app.add_handler(CommandHandler("profile", profile))
-    app.add_handler(CommandHandler("uploadpdf", uploadpdf))
+    app.add_handler(CommandHandler("generate", generate))
 
     app.add_handler(MessageHandler(filters.Document.PDF, receive_pdf))
 
