76bcbca6eab2be0bd4684a577c496f1128376530
ak4422920 <ak4422920@gmail.com>
2026-03-10 07:38:54 +0530

    Update commands.py

--- bot/commands.py
+++ bot/commands.py
@@ -1,4 +1,27 @@
+from database.mongo import users
+from utils.chat_register import register_chat
+
+
 async def start(update, context):
+
+    user = update.effective_user
+    chat = update.effective_chat
+
+    register_chat(chat)
+
+    existing = users.find_one({"user_id": user.id})
+
+    if not existing:
+
+        users.insert_one({
+            "user_id": user.id,
+            "name": user.first_name,
+            "username": user.username,
+            "language": "en",
+            "score": 0,
+            "badges": [],
+            "joined_at": str(update.message.date)
+        })
 
     await update.message.reply_text(
         "Welcome to Universal Quiz Bot!"
