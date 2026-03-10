72252261b0c78a2ee44578448ba9eac9af10c5b8
ak4422920 <ak4422920@gmail.com>
2026-03-10 07:41:37 +0530

    Update router.py

--- bot/router.py
+++ bot/router.py
@@ -1,8 +1,11 @@
-from telegram.ext import CommandHandler
+from telegram.ext import CommandHandler, CallbackQueryHandler
 from bot.commands import start, quiz
+from quiz_engines.inline_quiz import handle_answer
 
 
 def register_handlers(app):
 
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler("quiz", quiz))
+
+    app.add_handler(CallbackQueryHandler(handle_answer))
