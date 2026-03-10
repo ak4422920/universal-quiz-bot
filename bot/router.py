c70a1816112b1c066f755eb47aaaf3ef266727a9
ak4422920 <ak4422920@gmail.com>
2026-03-10 08:06:15 +0530

    Update router.py

--- bot/router.py
+++ bot/router.py
@@ -1,5 +1,5 @@
 from telegram.ext import CommandHandler, CallbackQueryHandler
-from bot.commands import start, quiz, challenge, createtournament, leaderboard
+from bot.commands import start, quiz, challenge, createtournament, leaderboard, elite, profile
 from quiz_engines.inline_quiz import handle_answer
 from challenges.challenge_handlers import accept_challenge
 from bot.commands import join_tournament_command
@@ -12,6 +12,8 @@ def register_handlers(app):
     app.add_handler(CommandHandler("challenge", challenge))
     app.add_handler(CommandHandler("createtournament", createtournament))
     app.add_handler(CommandHandler("leaderboard", leaderboard))
+    app.add_handler(CommandHandler("elite", elite))
+    app.add_handler(CommandHandler("profile", profile))
 
     app.add_handler(CallbackQueryHandler(handle_answer, pattern="^answer"))
     app.add_handler(CallbackQueryHandler(accept_challenge, pattern="^accept_challenge"))
